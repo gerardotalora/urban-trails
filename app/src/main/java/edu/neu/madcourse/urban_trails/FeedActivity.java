99ec7395acba1ad8e3d97f0edb492b309a037dc7
gerard <otaloracanovas.g@husky.neu.edu>
2020-07-25 19:10:01 -0400

    Home page fragments

--- app/src/main/java/edu/neu/madcourse/urban_trails/FeedActivity.java
+++ app/src/main/java/edu/neu/madcourse/urban_trails/FeedActivity.java
@@ -1,11 +1,20 @@
 package edu.neu.madcourse.urban_trails;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
-import android.view.View;
+import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import edu.neu.madcourse.urban_trails.fragments.HomeFragment;
+import edu.neu.madcourse.urban_trails.fragments.ProfileFragment;
+import edu.neu.madcourse.urban_trails.fragments.SearchFragment;
+import edu.neu.madcourse.urban_trails.fragments.StarFragment;
+import edu.neu.madcourse.urban_trails.fragments.StartTrailFragment;
 
 public class FeedActivity extends AppCompatActivity {
 
@@ -16,9 +25,40 @@ public class FeedActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feed);
         bottomNavigation = findViewById(R.id.bottom_navigation);
+
+        bottomNavigation.setOnNavigationItemSelectedListener(navigationItemSelectedListener);
+        openFragment(HomeFragment.newInstance("", ""));
     }
 
-    public void onClick(View view) {
-
+    public void openFragment(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.container, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
     }
+
+    BottomNavigationView.OnNavigationItemSelectedListener navigationItemSelectedListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                    switch (menuItem.getItemId()) {
+                        case R.id.navigation_home:
+                            openFragment(HomeFragment.newInstance("", ""));
+                            return true;
+                        case R.id.navigation_search:
+                            openFragment(SearchFragment.newInstance("", ""));
+                            return true;
+                        case R.id.navigation_start_trail:
+                            openFragment(StartTrailFragment.newInstance("", ""));
+                            return true;
+                        case R.id.navigation_star:
+                            openFragment(StarFragment.newInstance("", ""));
+                            return true;
+                        case R.id.navigation_profile:
+                            openFragment(ProfileFragment.newInstance("", ""));
+                            return true;
+                    }
+                    return false;
+                }
+            };
 }

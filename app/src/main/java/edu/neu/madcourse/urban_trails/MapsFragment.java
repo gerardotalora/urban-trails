539b5e45ac78c47d58780a705d386625009a931b
Matt Dailis <dailis@jpl.nasa.gov>
2020-07-31 09:43:17 -0700

    Request location permissions and show my location on map

--- app/src/main/java/edu/neu/madcourse/urban_trails/MapsFragment.java
+++ app/src/main/java/edu/neu/madcourse/urban_trails/MapsFragment.java
@@ -2,40 +2,37 @@ package edu.neu.madcourse.urban_trails;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsFragment extends Fragment {
+import static android.content.Context.LOCATION_SERVICE;
 
-    private OnMapReadyCallback callback = new OnMapReadyCallback() {
+public class MapsFragment extends Fragment implements OnMapReadyCallback, LocationListener {
 
-        /**
-         * Manipulates the map once available.
-         * This callback is triggered when the map is ready to be used.
-         * This is where we can add markers or lines, add listeners or move the camera.
-         * In this case, we just add a marker near Sydney, Australia.
-         * If Google Play services is not installed on the device, the user will be prompted to
-         * install it inside the SupportMapFragment. This method will only be triggered once the
-         * user has installed Google Play services and returned to the app.
-         */
-        @Override
-        public void onMapReady(GoogleMap googleMap) {
-            LatLng sydney = new LatLng(-34, 151);
-            googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-            googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-        }
-    };
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
+    GoogleMap map;
+    private LocationManager locationManager;
 
     @Nullable
     @Override
@@ -51,7 +48,109 @@ public class MapsFragment extends Fragment {
         SupportMapFragment mapFragment =
                 (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
         if (mapFragment != null) {
-            mapFragment.getMapAsync(callback);
+            mapFragment.getMapAsync(this);
         }
+        this.locationManager = (LocationManager) getActivity().getSystemService(LOCATION_SERVICE);
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera.
+     * In this case, we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to
+     * install it inside the SupportMapFragment. This method will only be triggered once the
+     * user has installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        this.map = googleMap;
+        LatLng sydney = new LatLng(-34, 151);
+        this.map.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        this.map.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        this.enableMyLocation();
+    }
+
+    private void enableMyLocation() {
+        if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_PERMISSION_REQUEST_CODE);
+            return;
+        }
+        this.map.setMyLocationEnabled(true);
+        subscribeToLocation();
+    }
+
+    private void zoomToLocation(double latitude, double longitude) {
+        LatLng latLng = new LatLng(latitude, longitude);
+        map.animateCamera(CameraUpdateFactory.newCameraPosition(CameraPosition.fromLatLngZoom(latLng, 16.0f)));
+    }
+
+    /**
+     * This method gets called whenever Android gives us an update on the user's location
+     *
+     * @param location
+     */
+    @Override
+    public void onLocationChanged(Location location) {
+        zoomToLocation(location.getLatitude(), location.getLongitude());
+    }
+
+    private void subscribeToLocation() {
+        Log.v("DEBUG", "subscribeToLocation");
+        Criteria criteria = new Criteria();
+        criteria.setAccuracy(Criteria.ACCURACY_COARSE);
+
+        String enabledProvider = locationManager.getBestProvider(criteria, false);
+
+        if (enabledProvider == null) {
+            Log.v("DEBUG", "Best provider is null");
+            return;
+        }
+
+        Log.v("DEBUG", "locationManager.requestSingleUpdate");
+        if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        locationManager.requestSingleUpdate(enabledProvider, this, null);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode,
+                permissions,
+                grantResults);
+        if (requestCode == LOCATION_PERMISSION_REQUEST_CODE) {
+            if (grantResults.length > 0
+                    && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                // Showing the toast message
+                Toast.makeText(getActivity(),
+                        "Fine Location Permission Granted",
+                        Toast.LENGTH_SHORT)
+                        .show();
+
+                enableMyLocation();
+            } else {
+                Toast.makeText(getActivity(),
+                        "Fine Location Permission Denied",
+                        Toast.LENGTH_SHORT)
+                        .show();
+            }
+        }
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
     }
 }

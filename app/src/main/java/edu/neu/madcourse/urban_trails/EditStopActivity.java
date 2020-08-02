9ab8813d366a2efddf6b99858bbd2bab9ed626cf
Matt Dailis <dailis@jpl.nasa.gov>
2020-08-02 13:55:38 -0700

    Open camera in edit stop activity

--- app/src/main/java/edu/neu/madcourse/urban_trails/EditStopActivity.java
+++ app/src/main/java/edu/neu/madcourse/urban_trails/EditStopActivity.java
@@ -3,14 +3,18 @@ package edu.neu.madcourse.urban_trails;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
 
 import edu.neu.madcourse.urban_trails.models.Stop;
 
 public class EditStopActivity extends AppCompatActivity {
 
+    private static final int REQUEST_IMAGE_CAPTURE = 1;
     private Stop stop;
     private EditText stopNameView;
 
@@ -34,6 +38,26 @@ public class EditStopActivity extends AppCompatActivity {
             intent.putExtra("bundle", b);
             setResult(RESULT_OK, intent);
             finish();
+        } else if (view.getId() == R.id.takePictureButton) {
+            // Open camera!
+
+            Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            }
+
         }
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            ImageView imageView = findViewById(R.id.imageView3);
+            imageView.setImageBitmap(imageBitmap);
+        }
+    }
+
 }

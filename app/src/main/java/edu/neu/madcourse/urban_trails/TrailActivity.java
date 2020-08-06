f08738bb1ccba8f8c18769270ae48559e079ef8f
Matt Dailis <dailis@jpl.nasa.gov>
2020-08-06 13:57:59 -0700

    Add Tap to edit to marker info

--- app/src/main/java/edu/neu/madcourse/urban_trails/TrailActivity.java
+++ app/src/main/java/edu/neu/madcourse/urban_trails/TrailActivity.java
@@ -3,14 +3,20 @@ package edu.neu.madcourse.urban_trails;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.bottomnavigation.BottomNavigationView.OnNavigationItemSelectedListener;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import edu.neu.madcourse.urban_trails.models.Stop;
 import edu.neu.madcourse.urban_trails.models.Trail;
@@ -18,6 +24,7 @@ import edu.neu.madcourse.urban_trails.models.Trail;
 public class TrailActivity extends AppCompatActivity implements OnNavigationItemSelectedListener, MapsFragmentContainerActivity {
 
     private MapsFragment mapsFragment;
+    private Map<Marker, View> lastInfoWindowView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +35,7 @@ public class TrailActivity extends AppCompatActivity implements OnNavigationItem
 
         BottomNavigationView bottomNavigation = findViewById(R.id.bottom_navigation);
         bottomNavigation.setOnNavigationItemSelectedListener(this);
+        this.lastInfoWindowView = new HashMap<>();
     }
 
     @Override
@@ -90,7 +98,36 @@ public class TrailActivity extends AppCompatActivity implements OnNavigationItem
     }
 
     @Override
-    public View getInfoContents(Marker marker) {
-        return null;
+    public View getInfoContents(final Marker marker) {
+        if (this.lastInfoWindowView != null) {
+            View res = this.lastInfoWindowView.get(marker);
+            if (res != null) {
+                this.lastInfoWindowView.put(marker, null);
+                return res;
+            }
+        }
+        View view = getLayoutInflater().inflate(R.layout.custom_info_window_create_trail, null);
+        TextView textView = view.findViewById(R.id.stopTitle2);
+        final ImageView imageView = view.findViewById(R.id.stopImage2);
+        if (marker.getTag() instanceof Stop) {
+            Stop stop = (Stop) marker.getTag();
+            textView.setText(stop.getTitle());
+            if (stop.getImageFileName() == null) {
+                imageView.setVisibility(View.GONE);
+            } else {
+                imageView.setVisibility(View.VISIBLE);
+                final Context context = this;
+//                String filename = Uri.fromFile(new File(stop.getImageFileName())).getLastPathSegment();
+                String filename = stop.getImageFileName();
+                this.lastInfoWindowView.put(marker, view);
+                Utils.displayThumbnail(getApplicationContext(), imageView, filename, new OnImageDrawnListener() {
+                    @Override
+                    public void onImageDrawn() {
+                        marker.showInfoWindow();
+                    }
+                }, 300, 300);
+            }
+        }
+        return view;
     }
 }
